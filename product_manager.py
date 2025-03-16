6a6710fae7e6b024ef752ef58bbeccbbdf02d446
Bogdan1555 <bogdanursu1516@gmail.com>
2025-03-16 19:04:42 +0200

    Added the details to the products

---
+++ product_manager.py
@@ -0,0 +1,17 @@
+from product import Product
+
+class ProductManager:
+    def __init__(self):
+        self.products = []
+
+    def add_product(self, product):
+        self.products.append(product)
+
+    def display_products(self):
+        return [product.display_info() for product in self.products]
+
+    def total_value(self):
+        return sum(product.price * product.quantity for product in self.products)
+
+def remove_product(self, product_name):
+    self.products = [product for product in self.products if product.name != product_name]
